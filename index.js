1780ac9fe7e287dbc6bd4c6b5fb5004968ddea6e
Alejandra <alejandrariscanevo@gmail.com>
2020-10-22 20:02:54 -0500

    prueba de cambio

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ class Enemy {
         //ctx.beginPath()
         // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         // ctx.fillStyle = this.color
-        // ctx.fill()  
+        ctx.fill()  
         ctx.drawImage(this.image, this.x, this.y, 100, 100)
     }
 
